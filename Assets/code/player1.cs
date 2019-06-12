a42d9c3ab57b151039bed06a112405c3ed920464
pena87 <45009922+pena87@users.noreply.github.com>
2019-06-11 23:10:37 -0500

    mi primer game

---
+++ Assets/code/player1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    public Rigidbody2D cerditoRB;
+    public float walkingSpeed;
+    public float jumpSpeed;
+    //public float maxvelovidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            cerditoRB.velocity += jumpSpeed * Vector2.up;
+
+        }
+
+
+
+        //float mover = Input.GetAxis("Horizontal");
+        //cerditoRB.velocity = new Vector2(mover * maxvelovidad, cerditoRB.velocity.y);
+    }
+}
