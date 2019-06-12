9e404c56c66800a0bbd44218c0ebbe5e67b37de3
pena87 <45009922+pena87@users.noreply.github.com>
2019-06-12 00:06:49 -0500

    paso nuevo

--- Assets/code/player1.cs
+++ Assets/code/player1.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class player1 : MonoBehaviour
 {
-    public Rigidbody2D cerditoRB;
+    Rigidbody2D cerditoRB;
+    public float maxVelocidad;
     public float walkingSpeed;
     public float jumpSpeed;
-    //public float maxvelovidad;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+
+        cerditoRB = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            cerditoRB.velocity += jumpSpeed * Vector2.up;
 
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { // x-axis movement
+            cerditoRB.velocity += jumpSpeed * Vector2.up;
+        }
+
+        { // x-axis movement
+            var v = cerditoRB.velocity;
+            var speed = 0f;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                speed += -walkingSpeed;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                speed += walkingSpeed;
+            }
+            v.x = speed;
+            cerditoRB.velocity = v;
         }
 
 
 
         //float mover = Input.GetAxis("Horizontal");
-        //cerditoRB.velocity = new Vector2(mover * maxvelovidad, cerditoRB.velocity.y);
+        //cerditoRB.velocity = new Vector2(mover * maxVelocidad, cerditoRB.velocity.y );
+        // cerditoRB.velocity = new Vector2(mover * maxVelovidad, cerditoRB.velocity.y);
     }
 }
